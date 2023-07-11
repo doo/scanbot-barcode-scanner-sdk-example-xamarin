a1b12964807a46aff92af2ec9e9afe3a1264296b
Marco <marco@scanbot.io>
2023-07-11 11:33:19 +0200

    Fixed NativeRenderer example build error in Release

--- NativeBarcodeSDKRenderer/NativeBarcodeSDKRenderer/NativeBarcodeSDKRenderer.Android/MainApplication.cs
+++ NativeBarcodeSDKRenderer/NativeBarcodeSDKRenderer/NativeBarcodeSDKRenderer.Android/MainApplication.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using Android.App;
 using Android.Runtime;
-using Android.Util;
-using IO.Scanbot.Sdk.Barcode_scanner;
-using NativeBarcodeSDKRenderer.Common;
-using ScanbotBarcodeSDK.Forms;
 using ScanbotBarcodeSDK.Forms.Droid;
 
 namespace NativeBarcodeSDKRenderer.Droid
@@ -12,12 +8,12 @@ namespace NativeBarcodeSDKRenderer.Droid
 #if DEBUG
     [Application(Debuggable = true, LargeHeap = true, Theme = "@style/MainTheme")]
 #else
-    [Application(Debuggable = false, LargeHeap = true, Theme = "@style/AppTheme")]
+    [Application(Debuggable = false, LargeHeap = true, Theme = "@style/MainTheme")]
 #endif
     public class MainApplication : Application
     {
 
-        static readonly string LOG_TAG = typeof(MainApplication).Name;
+        static readonly string LOG_TAG = nameof(MainApplication);
 
         // Use a custom temp storage directory for demo purposes.
         public static ScanbotOperations operations;
